9f5d2218a3ddbd1409a79bf6926f715f331c3f10
jyot53 <jyotsoni0053@gmail.com>
2023-12-22 10:52:46 +0530

    server: implemented get-current-user route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,12 +2,14 @@ const router = require('express').Router();
 const { 
     loginUser, 
     logoutUser, 
-    registerUser
+    registerUser,
+    getCurrentUser
 } = require("../controllers/user.controller.js");
 const { verifyJWT } = require("../middlewares/auth.middleware.js");
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT,  logoutUser);
+router.route("/get-user").get(verifyJWT,  getCurrentUser);
 
 module.exports = router;
