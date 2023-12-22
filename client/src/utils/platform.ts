e4ae2cbb205b9d95b4764b4e94b03f3f70467b17
jyot53 <jyotsoni0053@gmail.com>
2023-12-22 09:16:27 +0530

    client: utility functions

---
+++ client/src/utils/platform.ts
@@ -0,0 +1,36 @@
+import { TOKEN_NAME } from "./constants";
+
+export const getToken = () => {
+    return getClientCookie(TOKEN_NAME);
+}
+
+export const isLoggedIn = () => {
+    return !!getClientCookie(TOKEN_NAME);
+}
+
+export const getHeaders = (forceAuth:boolean) => {
+    const headers:any = {};
+    if(forceAuth || isLoggedIn()) {
+        headers['Authorization'] = 'Bearer ' + getToken();
+    }
+    headers['X-ThinkWik-Website'] = 'Thinkwik';
+    return headers;
+}
+
+export const getClientCookie = (cname:string) => {
+    try {
+        let ca = document.cookie.split(';');
+        let c, x;
+        for (let i = 0; i < ca.length; i++) {
+            c = ca[i];
+            x = ca[i].split('=');
+            if (x[0].trim() === cname) {
+            return x.slice(1).join('') || '';
+            }
+        }
+    return '';
+    } catch (error) {
+        console.error("Error getting client cookies ",error);
+        return '';
+    }
+}
