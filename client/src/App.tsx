f8c135ea33869e28b3c832ad30e062bc0325211c
jyot53 <jyotsoni0053@gmail.com>
2023-12-22 09:10:17 +0530

    client: added formik, axios package , routing - public & protected routes flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
+import {Routes,Route} from 'react-router-dom';
+import { Home } from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Edit from './components/Edit';
+import ProtectedRoute from './auth/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      Hello World!!!
-    </div>
+        <Routes>
+          {/* public routes */}
+          <Route path='/login' element={<Login/>}/>  
+          <Route path='/register' element={<Register/>}/>
+
+          {/* protected routes: */}
+          <Route path='/' element={<ProtectedRoute component={<Home/>} />} />
+          <Route path='/edit/:todoId' element={<ProtectedRoute component={<Edit/>} />} />
+      </Routes>
   );
 }
 
---
+++ client/src/auth/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const ProtectedRoute: React.FC<any> = ({ component: Component, ...rest }) => {
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return (
+    <Component {...rest} />
+  );
+};
+
+export default ProtectedRoute;
